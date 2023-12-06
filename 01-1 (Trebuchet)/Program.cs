6857b3173691dadb0216e33d84a752e41bec7aa1
Nyomdalee <139174287+Nyomdalee@users.noreply.github.com>
2023-12-06 02:52:09 +0100

    01-01 solution, with seasonally appropriate indentation

--- 01-1 (Trebuchet)/Program.cs
+++ 01-1 (Trebuchet)/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OneOne;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(GetSum());
+    }
+
+    private static int GetSum()
+    {
+        var lines = File.ReadAllLines($@"{AppDomain.CurrentDomain.BaseDirectory}\input.txt");
+        int sum = 0;
+
+        foreach (var line in lines)
+        {
+            int lineTotal = 0;
+            int lastNumber = 0;
+            foreach (var character in line)
+            {
+                if (char.IsNumber(character))
+                {
+                    lastNumber = int.Parse(character.ToString());
+                    if (lineTotal == 0)
+                        lineTotal = lastNumber * 10;
+                }
+            }
+            lineTotal += lastNumber;
+            sum += lineTotal;
+        }
+        return sum;
+    }
+}

cf04b2019ef18249fde567770e4dc6b56bb1848f
Nyomdalee <139174287+Nyomdalee@users.noreply.github.com>
2023-12-09 23:37:17 +0100

    07-1 solution (with an unhinged ctor)

---
+++ 07 (Camel Cards)/Hand.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Seven.Models;
+internal class Hand
+{
+    public string Cards { get; }
+    public int Bid { get; }
+    public ulong Fitness { get; }
+
+    //How not to write a ctor :)
+    public Hand(string cards, int bid)
+    {
+        Cards = cards;
+        Bid = bid;
+        Fitness = CalculateFitness();
+    }
+
+    private Dictionary<string, string> _cardValues = new()
+    {
+        { "A", "01" }, { "K", "02" }, { "Q", "03" }, { "J", "04" }, { "T", "05" }, { "9", "06" }, { "8", "07" },
+        { "7", "08" }, { "6", "09" }, { "5", "10" }, { "4", "11" }, { "3", "12" }, { "2", "13" }, { "1", "14" }
+    };
+
+    private ulong CalculateFitness()
+    {
+        var builder = new StringBuilder();
+        builder.Append(Cards.Distinct().Count().ToString());
+        builder.Append(6 - Cards.GroupBy(c => c).Select(a => a.Count()).Max()).ToString();
+
+        foreach (var card in Cards)
+        {
+            builder.Append(_cardValues[card.ToString()]);
+        }
+
+        return ulong.Parse(builder.ToString());
+    }
+}
---
+++ 07 (Camel Cards)/Program.cs
@@ -0,0 +1,25 @@
+﻿using Seven.Models;
+using System.Text.RegularExpressions;
+
+namespace Seven;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(GetSum());
+    }
+
+    private static int GetSum()
+    {
+        var lines = File.ReadAllLines("input.txt");
+
+        var Hands = lines
+            .Select(line => Regex.Matches(line, @"\w+"))
+            .Select(matches =>
+            new Hand(matches[0].Value, int.Parse(matches[1].Value)))
+            .OrderByDescending(h => h.Fitness).ToList();
+
+        return Hands.Select((h, i) => h.Bid * (i + 1)).Sum();
+    }
+}

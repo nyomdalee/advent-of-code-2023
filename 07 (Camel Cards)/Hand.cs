9c9ea4fb66c9b76521683a41fd949ee7be1d45d1
Nyomdalee <139174287+Nyomdalee@users.noreply.github.com>
2023-12-10 00:29:02 +0100

    07-2 solution, with an even more atrocious ctor, including nested ternary operators; fun

--- 07 (Camel Cards)/Hand.cs
+++ 07 (Camel Cards)/Hand.cs
@@ -17,15 +17,22 @@ internal class Hand
 
     private Dictionary<string, string> _cardValues = new()
     {
-        { "A", "01" }, { "K", "02" }, { "Q", "03" }, { "J", "04" }, { "T", "05" }, { "9", "06" }, { "8", "07" },
-        { "7", "08" }, { "6", "09" }, { "5", "10" }, { "4", "11" }, { "3", "12" }, { "2", "13" }, { "1", "14" }
+        { "A", "01" }, { "K", "02" }, { "Q", "03" }, { "T", "04" }, { "9", "05" }, { "8", "06" }, { "7", "07" },
+        { "6", "08" }, { "5", "09" }, { "4", "10" }, { "3", "11" }, { "2", "12" }, { "1", "13" }, { "J", "14" }
     };
 
     private ulong CalculateFitness()
     {
+        var jCount = Cards.Where(c => c == 'J').Count();
+
         var builder = new StringBuilder();
-        builder.Append(Cards.Distinct().Count().ToString());
-        builder.Append(6 - Cards.GroupBy(c => c).Select(a => a.Count()).Max()).ToString();
+        builder.Append((Cards.Distinct().Count() - (jCount > 0 ? jCount == 5 ? 0 : 1 : 0)).ToString());
+        builder.Append(6 - (Cards
+            .Where(c => c != 'J')
+            .GroupBy(c => c)
+            .Select(a => a.Count())
+            .DefaultIfEmpty(0)
+            .Max() + jCount)).ToString();
 
         foreach (var card in Cards)
         {

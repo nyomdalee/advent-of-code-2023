8ec4e6c9733794d2776919c49079b44ad535a39d
Nyomdalee <139174287+Nyomdalee@users.noreply.github.com>
2023-12-15 19:52:19 +0100

    11-2 solution

--- 11 (Cosmic Expansion)/Program.cs
+++ 11 (Cosmic Expansion)/Program.cs
@@ -13,56 +13,64 @@ public class Program
     private static long GetSum()
     {
         var lines = File.ReadAllLines("input.txt").ToList();
-        var expandedUniverse = ExpandColumns(ExpandRows(lines));
+        var expandedRows = GetExpandedRows(lines);
+        var expandedColumns = GetExpandedColumns(lines);
 
-        var galaxies = expandedUniverse
+        var galaxies = lines
         .SelectMany((l, i) => Regex.Matches(l, "#")
         .Select(m => new Point(i, m.Index))).ToList();
 
-        return GetSumOfDistances(galaxies);
+        return GetSumOfDistances(galaxies, expandedRows, expandedColumns);
     }
 
-    static List<string> ExpandRows(List<string> lines)
+    static List<int> GetExpandedRows(List<string> lines)
     {
-        List<string> expandedRows = new(lines);
-        int offset = 0;
+        List<int> expandedRows = new();
 
         for (int i = 0; i < lines.Count; i++)
         {
             if (!lines[i].Contains('#'))
-            {
-                expandedRows.Insert(i + offset, lines[i]);
-                offset++;
-            }
+                expandedRows.Add(i);
         }
         return expandedRows;
     }
 
-    static List<string> ExpandColumns(List<string> lines)
+    static List<int> GetExpandedColumns(List<string> lines)
     {
-        int offset = 0;
-        var expandedColumns = lines.Select(l => l.ToList()).ToList();
+        List<int> expandedColumns = new();
 
         for (int i = 0; i < lines[0].Length; i++)
         {
             if (!lines.Select(l => l[i]).Any(c => c == '#'))
-            {
-                for (int j = 0; j < lines.Count; j++)
-                    expandedColumns[j].Insert(i + offset, '.');
-
-                offset++;
-            }
+                expandedColumns.Add(i);
         }
-        return expandedColumns.Select(l => new string(l.ToArray())).ToList();
+        return expandedColumns;
     }
 
-    static int GetSumOfDistances(List<Point> galaxies)
+    static long GetSumOfDistances(List<Point> galaxies, List<int> expandedRows, List<int> expandedColumns)
     {
-        int sumOfPaths = 0;
+        int expansionCoefficient = 1000000;
+        long sumOfPaths = 0;
         for (int i = 0; i < galaxies.Count; i++)
         {
             for (int j = i + 1; j < galaxies.Count; j++)
-                sumOfPaths += Math.Abs(galaxies[i].X - galaxies[j].X) + Math.Abs(galaxies[i].Y - galaxies[j].Y);
+            {
+                // This can definitely be simplified, but I've run out of braincells for the day.
+                int bigX = galaxies[i].X > galaxies[j].X ? galaxies[i].X : galaxies[j].X;
+                int smolX = galaxies[i].X < galaxies[j].X ? galaxies[i].X : galaxies[j].X;
+
+                int bigY = galaxies[i].Y > galaxies[j].Y ? galaxies[i].Y : galaxies[j].Y;
+                int smolY = galaxies[i].Y < galaxies[j].Y ? galaxies[i].Y : galaxies[j].Y;
+
+                int basePath = (bigX - smolX) + (bigY - smolY);
+
+                int crossedRows = expandedRows.Where(r => r > smolX && r < bigX).Count();
+                int crossedColumns = expandedColumns.Where(c => c > smolY && c < bigY).Count();
+
+                long dilatedPath = (crossedRows + crossedColumns) * (expansionCoefficient - 1);
+
+                sumOfPaths += basePath + dilatedPath;
+            }
         }
         return sumOfPaths;
     }

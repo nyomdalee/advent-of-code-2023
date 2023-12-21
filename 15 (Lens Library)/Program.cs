f989ce91213b5fa6fac1e512a8b9027809c90093
Nyomdalee <139174287+Nyomdalee@users.noreply.github.com>
2023-12-21 19:25:14 +0100

    15-1 solution

---
+++ 15 (Lens Library)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fifteen;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(GetSum());
+    }
+
+    private static long GetSum()
+    {
+        var line = File.ReadAllText("input.txt");
+        var sequenceValues = line.Split(',');
+
+        return sequenceValues.Select(GetReindeerHash).Sum();
+    }
+
+    private static int GetReindeerHash(string inputString)
+    {
+        int currentValue = 0;
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            var asciiValue = (int)inputString[i];
+            currentValue += asciiValue;
+            currentValue *= 17;
+            currentValue %= 256;
+        }
+        return currentValue;
+    }
+}
